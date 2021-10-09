efe2a539fed906f8c1f9599f08fafd3460ebaa8a
Mohammad Kazem Saeedi <soulless7@protonmail.com>
2021-10-09 21:43:34 +0330

    fixed a bug

--- dist/textToImage.js
+++ dist/textToImage.js
@@ -57,7 +57,7 @@ const createTextData = (text, config, canvas) => {
             wordCount += 1;
         }
         const testLine = `${line} ${word}`.trim();
-        const testLineWidth = textContext.measureText(testLine).width;
+        const testLineWidth = textContext.measureText(testLine).width + (fontSize / 3);
         if (addNewLines.indexOf(n) > -1 || (testLineWidth > maxWidth && n > 0)) {
             textContext.fillText(line, textX, textY);
             line = word;
